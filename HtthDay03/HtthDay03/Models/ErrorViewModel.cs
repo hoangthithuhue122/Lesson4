342c57292775daafce79f29646e8a714074b07e6
hoangthithuhue122 <hoangthuthuhue87@gmail.com>
2026-09-12 20:16:47 +0700

    Delete HtthDay03 directory

--- HtthDay03/HtthDay03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HtthDay03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
